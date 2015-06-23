1e0941a585430815144b1429cc336e7e1fac5ca6
miguel-martin <miguelm@unizar.es>
2015-06-23 20:54:56 +0200

    Con BD sqlite

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,22 +1,30 @@
 // File: controllers/quiz_controller.js
 
+// modulo7
+var models = require('../models/models.js');
 
 // GET /author
 exports.author = function(req,res){
    res.render('author', {nombre: 'Miguel Martín', foto: '/images/foto-perfil.jpg'});
 }
 
+
 // GET /quizes/question
 exports.question = function(req,res){
-    res.render('quizes/question', {pregunta: 'Capital de Italia'});
+   models.Quiz.findAll().success(function(quiz){
+      res.render('quizes/question', { pregunta: quiz[0].pregunta })
+   })
 };
 
 
 // GET /quizes/answer
 exports.answer = function(req,res){
- if (req.query.respuesta === "Roma" ){
-    res.render('quizes/answer',{respuesta: 'Correcto'});
- }else{
-    res.render('quizes/answer',{respuesta: 'Incorrecto'});
- }
-}
+   models.Quiz.findAll().success(function(quiz){
+       if (req.query.respuesta === quiz[0].respuesta){
+           res.render('quizes/answer', {respuesta: 'Correcto'});
+       }
+       else{
+           res.render('quizes/answer', {respuesta: 'Incorrecto'});
+       }
+   })
+};
---
+++ models/quiz.js
@@ -0,0 +1,8 @@
+// Definición del módulo Quiz
+
+module.exports = function(sequelize, DataTypes){
+	return sequelize.define('Quiz',	
+                                { pregunta: DataTypes.STRING,
+                                  respuesta: DataTypes.STRING
+			        });
+}
