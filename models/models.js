54c0184faf621833c8b66b19a1a24661fec81b32
miguel-martin <miguelm@unizar.es>
2015-06-29 13:46:49 +0200

    Autoload implementado

--- models/models.js
+++ models/models.js
@@ -33,7 +33,8 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function() {
+//sequelize.sync().success(function() {
+sequelize.sync().then(function() {
   // success(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().success(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+// Filosofía DRY (don't repeat yourself): hacer todo lo común en el .load y después lo específico en cada función específica
+// Esta línea quiere decir que si existe el parámetro quizId en la ruta, invoque el autoload...
+router.param('quizId', quizController.load);  // autoload :quizId
+
 // Definición de rutas ...
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
