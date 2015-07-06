c1cf612a81ef06f994a620f859f8ca9f282c8fce
miguel-martin <miguelm@unizar.es>
2015-07-06 11:25:22 +0200

    Validacion comentarios

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res) {
 // Filosofía DRY (don't repeat yourself): hacer todo lo común en el .load y después lo específico en cada función específica
 // Esta línea quiere decir que si existe el parámetro quizId en la ruta, invoque el autoload...
 router.param('quizId', quizController.load);  // autoload :quizId
-
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -38,5 +38,7 @@ router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quiz
 
 router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+	                                    sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
